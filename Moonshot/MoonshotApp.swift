62117f25444496025d24df51d4d700345d91ef7b
cspeloso <chrispeloso2@gmail.com>
2022-02-19 13:13:24 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Chris Peloso on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
