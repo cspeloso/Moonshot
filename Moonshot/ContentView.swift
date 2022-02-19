222e191a063901a5d06ec1453c434bc8ec9ca0b2
cspeloso <chrispeloso2@gmail.com>
2022-02-19 17:32:52 -0500

    part 1

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,10 +7,58 @@
 
 import SwiftUI
 
+struct loginView: View{
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View{
+        
+        VStack{
+            Form{
+                Section{
+                    TextField("Username" ,text: $username)
+                }
+                Section{
+                    SecureField("Password", text: $password)
+                }
+                Section{
+                    HStack{
+
+                        Spacer()
+                        Button("Log In"){
+                            if username != "" && password != "" {
+                                dismiss()
+                            }
+                        }
+                        .frame(width: 50, height: 30, alignment: .center)
+                        
+                        Spacer()
+                    }
+
+                }
+            }
+            
+        }
+        
+    }
+    
+    init(){
+        print("test")
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            NavigationLink{
+                loginView()
+            } label:{
+                Text("Click here to sign in")
+                    .padding()
+            }
+            .navigationTitle("SwiftUI")
+        }
     }
 }
 
